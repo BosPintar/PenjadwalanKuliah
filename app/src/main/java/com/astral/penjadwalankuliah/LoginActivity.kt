4e148567348ea1bd1587698ff679d3196970083f
ahmadsupriadi <cahaya.supriady04@gmail.com>
2025-06-09 09:21:29 +0700

    Membuat menu utama

--- app/src/main/java/com/astral/penjadwalankuliah/LoginActivity.kt
+++ app/src/main/java/com/astral/penjadwalankuliah/LoginActivity.kt
@@ -2,9 +2,7 @@ package com.astral.penjadwalankuliah
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.astral.penjadwalankuliah.databinding.ActivityLoginBinding
 
@@ -18,18 +16,14 @@ class LoginActivity : AppCompatActivity() {
             val username = binding.edUsername.text.toString()
             val password = binding.edPass.text.toString()
             if (username.isEmpty() || password.isEmpty()) {
-                // Kalau username atau password kosong
                 Toast.makeText(this, "Username dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show()
             } else if (username == "admin" && password == "12345") {
-                // Kalau username dan password benar pindah form
                 val intent = Intent(this, MenuUtamaActivity::class.java)
+                intent.putExtra("nama", "Jailani")
                 startActivity(intent)
             } else {
-                // Kalau username atau password salah muncul pesan
                 Toast.makeText(this, "Username atau Password salah", Toast.LENGTH_SHORT).show()
             }
-
         }
-
     }
 }
--- app/src/main/java/com/astral/penjadwalankuliah/MenuUtamaActivity.kt
+++ app/src/main/java/com/astral/penjadwalankuliah/MenuUtamaActivity.kt
@@ -3,12 +3,14 @@ package com.astral.penjadwalankuliah
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.astral.penjadwalankuliah.databinding.ActivityMenuUtamaBinding
 
 class MenuUtamaActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMenuUtamaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_utama)
-
+        binding = ActivityMenuUtamaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.txtNama.text = intent.getStringExtra("nama")
     }
 }
